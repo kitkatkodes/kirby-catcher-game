b092d1fb57c8098eafceb4255ec6dbfcd7e6144e
kitkatkodes <shreyasikirti@gmail.com>
2025-03-08 21:54:38 +0530

    script.js

--- script.js
+++ script.js
@@ -6,15 +6,20 @@ let button;
 
 function preload() {
   // Preload images or sounds here
-  kirbyImg = loadImage('kirby.png');
-  backImg=loadImage('pls.jpg');
-  gameSound=loadSound('zelda.mp3');
+  kirbyImg = loadImage('kirby.png');  // ✅ Corrected path for GitHub Pages
+  backImg = loadImage('pls.jpg');  
+  gameSound = loadSound('zelda.mp3');
 }
 
-
 function setup() {
-  userStartAudio(); 
   createCanvas(400, 400);
+
+  // Require user interaction before starting sound
+  userStartAudio().then(() => {
+    gameSound.loop();
+  }).catch(err => {
+    console.log("Audio blocked until user interaction:", err);
+  });
 
   //##
   button = createButton('Retry');
@@ -30,22 +35,17 @@ function setup() {
   button.style('color', 'white');               // White text
   button.style('padding', '10px');              // Added padding
   button.style('border-radius', '5px');     
-  //
 
-/// added a call back for button
+  /// added a call back for button
   button.mousePressed(buttonClicked);
 
-  
-  
   catcher = new Sprite();
   catcher.img = kirbyImg;
-  catcher.scale=0.05;
+  catcher.scale = 0.05;
   catcher.collider = "k";
   catcher.pos.x = 200;
   catcher.pos.y = 378;
-  gameSound.loop()
   
-
   starText = `
   ........bbb...
   .....byyyb.....
@@ -64,84 +64,75 @@ function setup() {
 
   star = new Sprite();
   star.img = spriteArt(starText, 5);
-  star.scale=0.6;
+  star.scale = 0.6;
   star.vel = createVector(1, 3);
 }
 
 function buttonClicked() {
   //console.log("Button pressed")
-  score=0
-  button.hide()
-  star.pos.x=200
-  star.pos.y=20
+  score = 0;
+  button.hide();
+  star.pos.x = 200;
+  star.pos.y = 20;
   star.vel = createVector(1, 3);
 }
 
-
 /* DRAW LOOP REPEATS */
 function draw() {
-  background(229,229,229);
-  image(backImg,0,0);
+  background(229, 229, 229);
+  image(backImg, 0, 0);
 
-
-  
   // Draw directions to screen
   fill(0);
   fill(255, 255, 255);
   textSize(12);
   
-  text("Move the \ncatcher with the \nleft and right \narrow keys to \ncatch the falling \nobjects.", width-100, 20);
-  text("Game by- \nShreyasi Kirti",300,370);
+  text("Move the \ncatcher with the \nleft and right \narrow keys to \ncatch the falling \nobjects.", width - 100, 20);
+  text("Game by- \nShreyasi Kirti", 300, 370);
 
-  
-  
-// If fallingObject reaches bottom, move back to random position at top
+  // If fallingObject reaches bottom, move back to random position at top
   if (star.y >= 400) {
-    star.y=0;
-    score=score-1;
-    star.x=random(width);
-    star.vel.y=random(1,5);
-    if (score<=-1) {
+    star.y = 0;
+    score = score - 1;
+    star.x = random(width);
+    star.vel.y = random(1, 5);
+    if (score <= -1) {
       //console.log("YOU LOST!");
-      star.pos.x=200;
-      
-      star.pos.y=150;
-      star.vel = createVector(0,0);
-
-      button.show()
-      
-
+      star.pos.x = 200;
+      star.pos.y = 150;
+      star.vel = createVector(0, 0);
+      button.show();
     }
   }
-//Move catcher
-  if (kb.pressing("left")) {
-    catcher.vel.x=-3;}
-  else if (kb.pressing("right")) {
-    catcher.vel.x=3;}
-  else {
-    catcher.vel.x=0;}
-  //Stop catcher at edges of screen
+
+  // Move catcher
+  if (keyIsDown(LEFT_ARROW)) {
+    catcher.vel.x = -3;
+  } else if (keyIsDown(RIGHT_ARROW)) {
+    catcher.vel.x = 3;
+  } else {
+    catcher.vel.x = 0;
+  }
+
+  // Stop catcher at edges of screen
   if (catcher.x < 50) {
-    catcher.x=50;
+    catcher.x = 50;
+  } else if (catcher.x > 350) {
+    catcher.x = 350;
   }
-  else if (catcher.x>350) {
-    catcher.x=350;
-  }
-  //if fallingObject collides with catcher, move back to random position at top
+
+  // If fallingObject collides with catcher, move back to random position at top
   if (star.collides(catcher)) {
-    star.y=0;
-    star.x=random(width);
-    star.vel.y=random(1,5);
-    star.direction="down";
-    score=score+1;
-
-    
+    star.y = 0;
+    star.x = random(width);
+    star.vel.y = random(1, 5);
+    star.direction = "down";
+    score = score + 1;
   }
-  
 
-// show score at top left part of screen
-textSize(12);
-text('Score = ' + score,10,10);
-
+  // Show score at top left part of screen
+  textSize(12);
+  text('Score = ' + score, 10, 10);
 }
+
 //Code written by Shreyasi K.
